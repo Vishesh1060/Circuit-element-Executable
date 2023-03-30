8a44cf139b720bb22e9ccd3de3bd64f5c6593985
Vishesh1060 <73687610+Vishesh1060@users.noreply.github.com>
2023-03-30 17:45:20 +0530

    Base commit
    Setting everything up.

---
+++ MLv2/test_frcnn.py
@@ -0,0 +1,89 @@
+import os
+import cv2
+import numpy as np
+import sys
+import pickle
+import time
+import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+from keras.backend.tensorflow_backend import set_session
+from optparse import OptionParser
+from keras_frcnn import config
+from tensorflow.keras import backend as K
+from keras_frcnn import roi_helpers
+from keras_frcnn import resnet as nn
+
+sys.setrecursionlimit(40000)
+
+tf.compat.v1.disable_eager_execution()
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+config.log_device_placement = True
+sess = tf.compat.v1.Session(config=config)
+set_session(sess)
+
+parser = OptionParser()
+parser.add_option("-p", "--path", dest="test_path", help="Path to test data.")
+parser.add_option("-n", "--num_rois", type="int", dest="num_rois",
+                help="Number of ROIs per iteration. Higher means more memory use.", default=32)
+parser.add_option("--config_filename", dest="config_filename", help=
+                "Location to read the metadata related to the training (generated when training).",
+                default="config.pickle")
+parser.add_option("--network", dest="network", help="Base network to use. Supports vgg or resnet50.", default='resnet50')
+(options, args) = parser.parse_args()
+
+if not options.test_path:   # if filename is not given
+    parser.error('Error: path to test data must be specified. Pass --path to command line')
+
+config_output_filename = options.config_filename
+
+with open(config_output_filename, 'rb') as f_in:
+    C = pickle.load(f_in)
+
+C.use_horizontal_flips = False
+C.use_vertical_flips = False
+C.rot_90 = False
+
+img_path = options.test_path
+
+def format_img_size(img, C):
+    img_min_side = float(C.im_size)
+    (height, width, _) = img.shape
+        
+    if width <= height:
+        ratio = img_min_side/width
+        new_height = int(ratio * height)
+        new_width = int(img_min_side)
+    else:
+        ratio = img_min_side/height
+        new_width = int(ratio * width)
+        new_height = int(img_min_side)
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    return img, ratio    
+
+def format_img_channels(img, C):
+    img = img[:, :, (2, 1, 0)]
+    img = img.astype(np.float32)
+    img[:, :, 0] -= C.img_channel_mean[0]
+    img[:, :, 1] -= C.img_channel_mean[1]
+    img[:, :, 2] -= C.img_channel_mean[2]
+    img /= C.img_scaling_factor
+    img = np.transpose(img, (2, 0, 1))
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def format_img(img, C):
+    img, ratio = format_img_size(img, C)
+    img = format_img_channels(img, C)
+    return img, ratio
+
+def get_real_coordinates(ratio, x1, y1, x2, y2):
+    real_x1 = int(round(x1 // ratio))
+    real_y1 = int(round(y1 // ratio))
+    real_x2 = int(round(x2 // ratio))
+    real_y2 = int(round(y2 // ratio))
+    return (real_x1, real_y1, real_x2, real_y2)
+
+class_mapping = C
